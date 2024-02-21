f7b37631c51ed371ccaf1c0ae63b459d89b6a12d
nwalczak99 <113116376+nwalczak99@users.noreply.github.com>
2024-02-21 12:35:25 -0500

    setup gradebook

--- Gradebook.java
+++ Gradebook.java
@@ -1,2 +1,15 @@
 public class Gradebook {
+    private String studentName;
+
+    private double grade;
+    private String assignment;
+
+    public Gradebook()
+    {
+
+    }
+    public void createStudent(String name)
+    {
+
+    }
 }
