4429be9fbf5b428c93714d3f2a065ac7f590164a
Aamir Haniff <113187563+AamirHaniff12@users.noreply.github.com>
2024-02-21 12:43:56 -0500

    some done

--- Student.java
+++ Student.java
@@ -1,3 +1,29 @@
 public class Student {
 
+    public Student(String name)
+    {
+
+    }
+    public void addAssignment(String assignName)
+    {
+
+    }
+    public void gradeAssignments(String assignment, double grade)
+    {
+
+    }
+
+    public double getAssignment(String assignment)
+    {
+
+    }
+
+    public double getOverallGrade()
+    {
+
+    }
+    public String toString()
+    {
+
+    }
 }
