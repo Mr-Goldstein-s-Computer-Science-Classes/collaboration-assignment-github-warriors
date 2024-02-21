6511cc0ca18263bc6566aeef575e588ec06a2cc4
nwalczak99 <113116376+nwalczak99@users.noreply.github.com>
2024-02-21 12:43:10 -0500

    update

--- Gradebook.java
+++ Gradebook.java
@@ -2,14 +2,21 @@ public class Gradebook {
     private String studentName;
 
     private double grade;
-    private String assignment;
-
+    private ArrayList<String> assignments = new ArrayList<String>();
+    private ArrayList<Student> grades = new ArrayList<Student>();
     public Gradebook()
     {
 
     }
     public void createStudent(String name)
     {
-
+        Student students = new Student(name);
     }
+
+    public void addAssignment(String assignment)
+    {
+       assignments.add(assignment);
+    }
+    public void gradeAssignment(String assignment,String studentName, double grade)
+
 }
