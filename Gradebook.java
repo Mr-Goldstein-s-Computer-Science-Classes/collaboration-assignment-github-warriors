a647887d6187f4e48413d77cf87b0bb204a3b2ce
nwalczak99 <113116376+nwalczak99@users.noreply.github.com>
2024-02-22 14:00:31 -0500

    final gradebook update

--- Gradebook.java
+++ Gradebook.java
@@ -4,95 +4,150 @@ public class Gradebook {
 
     private List<Student> students = new ArrayList<Student>();
     private ArrayList<String> assignments = new ArrayList<String>();
-    public Gradebook()
-    {
+
+    public Gradebook() {
 
     }
-    public void createStudent(String name)
-    {
+
+    public void createStudent(String name) {
         students.add(new Student(name));
     }
 
-    public void addAssignment(String assignment)
-    {
+    public void addAssignment(String assignment) {
         assignments.add(assignment);
-        for(Student student:students){
+        for (Student student : students) {
             student.addAssignment(assignment);
         }
     }
-    public void gradeAssignment(String assignment,String studentName, double grade)
-    {
-        for(Student student:students)
-        {
-            if(studentName.equals(student.toString().split("-")[0])){
+
+    public void gradeAssignment(String assignment, String studentName, double grade) {
+        for (Student student : students) {
+            if (studentName.equals(student.toString().split("-")[0])) {
                 student.gradeAssignment(assignment, grade);
                 break;
             }
         }
 
     }
-    public double getAverageGrade(String assignment)
-    {
+
+    public double getAverageGrade(String assignment) {
         double sum = 0;
         int count = 0;
-        for(Student student:students)
-        {
+        for (Student student : students) {
             sum += student.getAssignmentGrade(assignment);
             count++;
         }
-        if(count > 0){
-            return sum/count;
-        }
-        else{
+        if (count > 0) {
+            return sum / count;
+        } else {
             return 0.0;
         }
     }
-    public double getMinimumGrade(String assignment)
-    {
+
+    public double getMinimumGrade(String assignment) {
         double minGrade = Double.MAX_VALUE;
-        for(Student student: students)
-        {
+        for (Student student : students) {
             double grade = student.getAssignmentGrade(assignment);
-            if(grade < minGrade && grade > 0)
-            {
+            if (grade < minGrade && grade > 0) {
                 minGrade = grade;
             }
         }
-        if(minGrade == Double.MAX_VALUE) {
+        if (minGrade == Double.MAX_VALUE) {
             return 0.0;
-        }
-        else {
+        } else {
             return minGrade;
         }
 
     }
-    public double getMaximumGrade(String assignment)
-    {
-        double maxGrade = double.MIN_VALUE;
-        for(Student student:students)
-        {
-            double grade = 
+
+    public double getMaximumGrade(String assignment) {
+        double maxGrade = Double.MIN_VALUE;
+        for (Student student : students) {
+            double grade = student.getAssignmentGrade(assignment);
+            if (grade > maxGrade) {
+                maxGrade = grade;
+            }
+        }
+        if (maxGrade == Double.MIN_VALUE) {
+            return 0.0;
+        } else {
+            return maxGrade;
         }
 
     }
-    public double getAverageOverall()
-    {
+
+    public double getAverageOverall() {
+        double sum = 0;
+        int count = 0;
+        for (Student student : students) {
+            sum += student.getOverallGrade();
+            count++;
+        }
+        if (count > 0) {
+            return sum / count;
+        } else {
+            return 0.0;
+        }
 
     }
-    public double getMaxOverall()
-    {
+
+    public double getMaxOverall() {
+        double maxGrade = Double.MIN_VALUE;
+        for (Student student : students) {
+            double grade = student.getOverallGrade();
+            if (grade > maxGrade) {
+                maxGrade = grade;
+            }
+        }
+        if (maxGrade == Double.MIN_VALUE) {
+            return 0.0;
+        } else {
+            return maxGrade;
+        }
 
     }
-    public double getMinOverall()
-    {
+
+    public double getMinOverall() {
+        double minGrade = Double.MAX_VALUE;
+        for (Student student : students) {
+            double grade = student.getOverallGrade();
+            if (grade < minGrade && grade > 0) {
+                minGrade = grade;
+            }
+        }
+        if (minGrade == Double.MAX_VALUE) {
+            return 0.0;
+        } else {
+            return minGrade;
+        }
 
     }
-    public double getStudentsGrade(String name)
-    {
 
+    public double getStudentsGrade(String studentName) {
+        for (Student student : students) {
+            if (studentName.equals(student.toString().split("-")[0])) {
+                return student.getOverallGrade();
+            }
+        }
+        return 0.0;
     }
 
+    public String toString() {
+        StringBuilder result = new StringBuilder("Gradebook:\n");
+        for (Student student : students) {
+            result.append(student.toString()).append("\n");
+        }
+        return result.toString();
+    }
 
-
+    public Student getStudent(String studentName) {
+        for (Student student : students) {
+            if (studentName.equals(studentName.toString().split("-")[0])) ;
+            {
+                return student;
+            }
+        }
+            return null;
+}
 
 }
