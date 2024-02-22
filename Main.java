9e4c7461ca50f7bcb056b44d2afaa21de91edcf6
Aamir Haniff <113187563+AamirHaniff12@users.noreply.github.com>
2024-02-22 13:18:56 -0500

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        Gradebook grades = new Gradebook();
+
+        grades.createStudent("Tyler");
+        grades.createStudent("James");
+        grades.createStudent("Ethan");
+        grades.createStudent("Scotty");
+        grades.createStudent("David");
+        grades.createStudent("Patrick");
+
+        grades.addAssignment("Stacks And Queues Station Activity");
+        grades.addAssignment("Stacks and queues programs");
+        grades.addAssignment("Stacks and queues quiz");
+        grades.addAssignment("Github Exploration");
+
     }
 }
